1668dcad9732cefb57654341f9e952f952e80f28
Hawkeon <ptiendat487@gmail.com>
2025-09-03 15:24:16 +0700

    add bai tap giao 3/9

--- LamQuenClass/Main.java
+++ LamQuenClass/Main.java
@@ -1,6 +1,29 @@
 package BaiTapVeNha.LamQuenClass;
 
 public class Main {
+    class SinhVien { 
+        private String maSv , hoTen ; 
+        private int tuoi ;
+        private static int soLuong = 0 ; 
+        public SinhVien(String maSv, String hoTen, int tuoi) {
+            this.maSv = maSv;
+            this.hoTen = hoTen;
+            this.tuoi = tuoi;
+            soLuong++;
+    }
+        public void setThongTin(String maSv, String hoTen, int tuoi) {
+            this.maSv = maSv;
+            this.hoTen = hoTen;
+            this.tuoi = tuoi;
+    }
+        public String HienThiThongTIn ( ) { 
+            return this.maSv+" " +this.hoTen  +" " + this.tuoi ;
+
+        }
+        public int HienThiSoLuong () { 
+            return soLuong ;
+        }
+    }
     public static void main(String[] args) {
         
     }
